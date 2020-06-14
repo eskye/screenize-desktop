63d6187ff7102b60320ac01c15444bc903926112
eskye <sunkanmiijatuyi@gmail.com>
2020-06-14 23:37:40 +0100

    added extension signature

--- screenize/src/render.js
+++ screenize/src/render.js
@@ -1,29 +1,48 @@
 const { desktopCapturer, remote } = require("electron");
 const { Menu, dialog } = remote;
-const { writeFile } = require("fs");
+const { writeFileSync } = require("fs"); 
+const notifier = require('node-notifier');
+const path = require('path');
 
 const videoElement = document.querySelector("video");
 const videoSelectBtn = document.getElementById("videoSelectBtn");
+const stopBtn = document.getElementById("stopBtn");
+const startBtn = document.getElementById("startBtn");
+const fileNameTextField = document.getElementById('filenameTextField');
+
 //Get available video sources
 let mediaRecorder;
-const recordedChunks = [];
+let recordedChunks = [];
 
-const startBtn = document.getElementById("startBtn");
-startBtn.onclick = (e) => {
-  mediaRecorder.start();
+
+startBtn.onclick = (e) => { 
+    mediaRecorder.start();
+    fileNameTextField.style.display = 'none'
   startBtn.classList.add("is-danger");
   startBtn.innerText = "Recording";
+  notifier.notify ({
+    title: 'My awesome title',
+    message: 'Hello from electron, Mr. User!',
+    icon: path.join(__dirname, 'sm-logo.png'),  // Absolute path 
+    sound: true,  // Only Notification Center or Windows Toasters
+    wait: true    // Wait with callback, until user action is taken  
+ 
+ }, (err, response) => {
+    // Response is response from notification
+ });
 };
 
-const stopBtn = document.getElementById("stopBtn");
+ 
 
 stopBtn.onclick = (e) => {
-  mediaRecorder.stop();
+   mediaRecorder.stop();
+   fileNameTextField.style.display = 'block'
   startBtn.classList.remove("is-danger");
   startBtn.innerText = "Start";
 };
 
 const videoSources = async () => {
+
   const inputSources = await desktopCapturer.getSources({
     types: ["window", "screen"],
   });
@@ -55,12 +74,7 @@ const selectSource = async (source) => {
   };
   //Create audio stream
   const constraintsAudio = {
-    audio: {
-      echoCancellation: false,
-      channelCount: 1,
-      autoGainControl: false,
-      noiseSuppression: false,
-    },
+    audio: true
   };
   //Create stream
   const videoStream = await navigator.mediaDevices.getUserMedia(
@@ -74,35 +88,88 @@ const selectSource = async (source) => {
     ...audioStream.getAudioTracks(),
   ]);
   videoElement.srcObject = combinedStream;
+  videoElement.muted = true;
+  videoElement.volume = 0;
   videoElement.play();
 
-  const videoOutputOptions = { mimeType: "video/webm; codecs=vp9" };
-  mediaRecorder = new MediaRecorder(combinedStream, videoOutputOptions);
+  let videoOutputOptions = {
+      mimeType: 'video/webm;codecs=vp9',
+     audioBitsPerSecond: 128000,
+    videoBitsPerSecond: 2500000,
+    
+    };
 
-  //Register event handlers
-  mediaRecorder.ondataavailable = handleDataAvailable;
+  if (!MediaRecorder.isTypeSupported(videoOutputOptions.mimeType)) {
+    console.error(`${videoOutputOptions.mimeType} is not supported`);
+    videoOutputOptions.mimeType = 'video/webm;codecs=vp8,opus';
+    if (!MediaRecorder.isTypeSupported(videoOutputOptions.mimeType)) {
+      console.error(`${videoOutputOptions.mimeType} is not supported`);
+      videoOutputOptions.mimeType = 'video/webm';
+      if (!MediaRecorder.isTypeSupported(videoOutputOptions.mimeType)) {
+        console.error(`${videoOutputOptions.mimeType} is not supported`);
+        videoOutputOptions.mimeType = 'video/webm;codecs=H264';
+      }
+    }
+  } 
+  try {   
+    mediaRecorder = new MediaRecorder(combinedStream, videoOutputOptions);
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder:', e);
+   // errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+    return;
+  } 
+
+ // Register event handlers
+ mediaRecorder.ondataavailable = handleDataAvailable;
   mediaRecorder.onstop = handleStop;
 };
 
-const handleDataAvailable = (e) => {
-  console.log("video availabe: " + e);
-  recordedChunks.push(e.data);
+const handleDataAvailable = (event) => {
+    console.log('handleDataAvailable', event);
+    if (event.data && event.data.size > 0) {
+      recordedChunks.push(event.data);
+    }
 };
 
-const handleStop = async (e) =>{
-    const blob = new Blob(recordedChunks, {
-      type: 'video/webm; codecs=vp9'
-    });
-  
-    const buffer = Buffer.from(await blob.arrayBuffer());
-  
-    const { filePath } = await dialog.showSaveDialog({
-      buttonLabel: 'Save video',
-      defaultPath: `vid-${Date.now()}.webm`
-    });
-  
-    if (filePath) {
-      writeFile(filePath, buffer, () => console.log('video saved successfully!'));
+const handleStop = async (e) =>{ 
+
+        const rand =  Math.floor((Math.random() * 10000000));
+	    const name  = "video_"+Date.now()+"-"+rand+".webm" ; 
+        const blob = new Blob(recordedChunks, { type: 'video/webm'});
+        const blobraw = await blob.arrayBuffer();
+        const buffer = Buffer.from(blobraw);
+        const { filePath } = await dialog.showSaveDialog(
+            {
+               buttonLabel: 'Save video',
+               defaultPath: name,
+               filters :[
+                {name: 'webm', extensions: ['webm',]},
+                {name: 'All Files', extensions: ['*']}
+               ]
+            }
+            );
+        if (filePath) {
+            await writeFileSync(filePath, buffer,writeFileCallback );
+        }
     }
-  
-  }
+    
+
+const writeFileCallback = () => {
+    recordedChunks = [];
+    console.log("Video saved successfully");
+    stopVideo();
+}
+
+ 
+const stopVideo = () => {
+    videoElement.src = videoElement.srcObject = null;
+    videoElement.muted = false;
+    videoElement.volume = 1;
+}
+
+
+
+
+
+ 
+
